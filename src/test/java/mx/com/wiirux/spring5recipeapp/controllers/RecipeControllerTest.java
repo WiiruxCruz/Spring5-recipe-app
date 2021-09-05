fcc7838dd43049108d5d74ac834350a3f37aceda
WiiruxCruz <hialf.92@gmail.com>
2021-09-04 23:39:26 -0500

    Cambio para utilizar argumentCaptor y hacer la prueba correctamente

--- src/test/java/mx/com/wiirux/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/mx/com/wiirux/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -3,15 +3,21 @@ package mx.com.wiirux.spring5recipeapp.controllers;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import mx.com.wiirux.spring5recipeapp.domain.Receta;
 import mx.com.wiirux.spring5recipeapp.services.RecetaService;
 
 class RecipeControllerTest {
@@ -33,11 +39,29 @@ class RecipeControllerTest {
 
 	@Test
 	void testListaRecetas() {
+		
+		//dado
+		Set<Receta> recetas = new HashSet<>();
+		recetas.add(new Receta());
+		
+		Receta receta = new Receta();
+		receta.setId(1L);
+		recetas.add(receta);
+		
+		when(rs.getRecetas()).thenReturn(recetas);
+		
+		ArgumentCaptor<Set<Receta>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+		
+		//cuando
 		String viewName = rc.listaRecetas(model);
+		
+		//entonces
 		assertEquals("receta/index", viewName);
 		verify(rs, times( 1 ) ).getRecetas();
-		verify( model, times(1) ).addAttribute(Mockito.eq("recetas"), Mockito.anySet() );
-		
+		//verify( model, times(1) ).addAttribute(Mockito.eq("recetas"), Mockito.anySet() );
+		verify( model, times(1) ).addAttribute(Mockito.eq("recetas"), argumentCaptor.capture() );
+		Set<Receta> setRecetaController = argumentCaptor.getValue();
+		assertEquals(2,  setRecetaController.size());
 	}
 
 }
